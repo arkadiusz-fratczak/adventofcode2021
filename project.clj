d07c3eeb435a673e4fd0ce2752e1de2ffddb9a96
Arkadiusz Fratczak <arkadiusz.ef@gmail.com>
2021-12-01 22:47:58 +0100

    Initial commit: day 1

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject advent-of-code-2021 "0.1.0-SNAPSHOT"
+  :description "flatten-array exercise."
+  :resource-paths ["resources"]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :plugins [[lein-cljfmt "0.7.0"]])
