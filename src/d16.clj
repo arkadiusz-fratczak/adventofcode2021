fb634cc85393c60cf2ba8d59f6e6c659943bc778
Arkadiusz Fratczak <arkadiusz.ef@gmail.com>
2021-12-17 17:20:10 +0100

    Solution: day 17

--- src/d16.clj
+++ src/d16.clj
@@ -131,9 +131,9 @@
   ([p]
    (let [pl (parse-literal [] (:d p))]
      (merge p
-          {:literal (to-long (flatten pl))
-           :b (concat (:b p) (take (* 5 (count pl)) (:d p)))
-           :d (drop (take (* 5 (count pl))) (:d p))})))
+            {:literal (to-long (flatten pl))
+             :b (concat (:b p) (take (* 5 (count pl)) (:d p)))
+             :d (drop (take (* 5 (count pl))) (:d p))})))
   ([bits data]
    (let [to-parse (take 5 data)]
      (if (= \0 (first to-parse))
@@ -142,30 +142,28 @@
 
 (defn parse-length-type [p]
   (let [ltype (first (:d p))]
-    (merge p {
-            :length-type ltype
-            :counter (if (= \0 ltype) (to-int (rest (take 16 (:d p))))
-                                      (to-int (rest (take 12 (:d p)))))
-            :b (concat (:b p) (take (if (= \0 ltype) 16 12) (:d p)))
-            :d (drop (if (= \0 ltype) 16 12) (:d p))
-            })))
+    (merge p {:length-type ltype
+              :counter (if (= \0 ltype) (to-int (rest (take 16 (:d p))))
+                           (to-int (rest (take 12 (:d p)))))
+              :b (concat (:b p) (take (if (= \0 ltype) 16 12) (:d p)))
+              :d (drop (if (= \0 ltype) 16 12) (:d p))})))
 
 (declare parse-packet)
 
 (defn parse-subpackets
   ([p]
-  (let [parsed-subp (parse-subpackets [] (:length-type p) (:counter p) (:d p))]
-    (merge p
-         {:subpackets parsed-subp
-          :b (apply concat (:b p) (map :b parsed-subp))
-          :d (drop (reduce + (map #(count (:b %)) parsed-subp)) (:d p))})))
+   (let [parsed-subp (parse-subpackets [] (:length-type p) (:counter p) (:d p))]
+     (merge p
+            {:subpackets parsed-subp
+             :b (apply concat (:b p) (map :b parsed-subp))
+             :d (drop (reduce + (map #(count (:b %)) parsed-subp)) (:d p))})))
   ([subp lt counter data]
    (if (> counter 0)
      (let [sp (parse-packet data)
            sp-length (count (:b sp))]
        (if (= \0 lt)
-        (recur (conj subp sp) lt (- counter sp-length) (drop sp-length data))
-        (recur (conj subp sp) lt (dec counter) (drop sp-length data))))
+         (recur (conj subp sp) lt (- counter sp-length) (drop sp-length data))
+         (recur (conj subp sp) lt (dec counter) (drop sp-length data))))
      subp)))
 
 (defn parse-operator [p]
@@ -234,14 +232,14 @@
 
 (defn calculate-value [packet]
   (case (:type packet)
-        4 (:literal packet)
-        0 (apply + (map calculate-value (:subpackets packet)))
-        1 (apply * (map calculate-value (:subpackets packet)))
-        2 (apply min (map calculate-value (:subpackets packet)))
-        3 (apply max (map calculate-value (:subpackets packet)))
-        5 (if (apply > (map calculate-value (:subpackets packet))) 1 0)
-        6 (if (apply < (map calculate-value (:subpackets packet))) 1 0)
-        7 (if (apply = (map calculate-value (:subpackets packet))) 1 0)))
+    4 (:literal packet)
+    0 (apply + (map calculate-value (:subpackets packet)))
+    1 (apply * (map calculate-value (:subpackets packet)))
+    2 (apply min (map calculate-value (:subpackets packet)))
+    3 (apply max (map calculate-value (:subpackets packet)))
+    5 (if (apply > (map calculate-value (:subpackets packet))) 1 0)
+    6 (if (apply < (map calculate-value (:subpackets packet))) 1 0)
+    7 (if (apply = (map calculate-value (:subpackets packet))) 1 0)))
 
 (defn answer2 []
   (let [d16 (parse (read-data-safe "resources/d16.txt"))]
