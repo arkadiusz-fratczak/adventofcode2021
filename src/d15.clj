db046b325f53cb5f17cb9d805cac2fe5569ef4b4
Arkadiusz Fratczak <arkadiusz.ef@gmail.com>
2021-12-20 14:29:53 +0100

    Solution: day 15 part 2

--- src/d15.clj
+++ src/d15.clj
@@ -1,6 +1,7 @@
 (ns d15
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 ;--- Day 15: Chiton ---
 ;
@@ -23,16 +24,16 @@
 ;
 ;Your goal is to find a path with the lowest total risk. In this example, a path with the lowest total risk is highlighted here:
 ;
-;1163751742
-;1381373672
-;2136511328
-;3694931569
-;7463417111
-;1319128137
-;1359912421
-;3125421639
-;1293138521
-;2311944581
+;"1163751742"
+;"1381373672"
+;"2136511328"
+;"3694931569"
+;"7463417111"
+;"1319128137"
+;"1359912421"
+;"3125421639"
+;"1293138521"
+;"2311944581"
 ;
 ;The total risk of this path is 40 (the starting position is never entered, so its risk is not counted).
 ;
@@ -51,30 +52,29 @@
        (mapv #(Integer/parseInt %))))
 
 (defn find-neighbours [idx grid-size]
-  (cond-> []
-          (>= (- idx grid-size) 0)
-          (conj (- idx grid-size))
+  (cond-> #{}
+    (>= (- idx grid-size) 0)
+    (conj (- idx grid-size))
 
-          (< (+ idx grid-size) (* grid-size grid-size))
-          (conj (+ idx grid-size))
+    (< (+ idx grid-size) (* grid-size grid-size))
+    (conj (+ idx grid-size))
 
-          (> (rem idx grid-size) 0)
-          (conj (dec idx))
+    (> (rem idx grid-size) 0)
+    (conj (dec idx))
 
-          (< (rem idx grid-size) (dec grid-size))
-          (conj (inc idx))))
+    (< (rem idx grid-size) (dec grid-size))
+    (conj (inc idx))))
 
 (defn to-graph [data size]
   (->> data
        (map-indexed (fn [i _] {i (find-neighbours i size)}))
        (apply merge)))
 
-(defn find-next-node [node-costs processed]
-  (key (apply min-key val
-              (apply dissoc node-costs processed))))
+(defn find-next-node [node-costs]
+  (key (apply min-key val node-costs)))
 
 (defn update-costs [node-costs node parent data]
-  (let [current-cost (or (get node-costs node) ##Inf)
+  (let [current-cost (or (get node-costs node) 999999)
         parent-cost (get node-costs parent)
         new-cost (+ parent-cost (nth data node))]
     (if (< new-cost current-cost)
@@ -84,25 +84,25 @@
 (defn dijkstra [start graph data]
   (let [counter (atom 1)
         start-nbrs (get graph start)
-        start-costs (reduce (fn [acc n] (assoc acc n (nth data n))) {} start-nbrs)
-        start-node (key (apply min-key val start-costs))]
+        start-dist (reduce (fn [acc n] (assoc acc n (nth data n))) {} start-nbrs)
+        start-node (key (apply min-key val start-dist))]
     (loop [node-idx start-node
-           node-nbrs (get graph node-idx)
-           node-costs start-costs
-           processed #{}]
+           node-nbrs (set/difference (get graph start-node) #{start})
+           node-dist start-dist
+           processed #{start}]
       (if (= node-idx (dec (count data)))
-        node-costs
-        (let [;_ (println (swap! counter inc) "/" (count data))
-              updated-costs (reduce
-                              (fn [acc n] (update-costs acc n node-idx data))
-                              node-costs
-                              node-nbrs)
+        node-dist
+        (let [_ (println (swap! counter inc) "/" (count data))
+              updated-costs (dissoc (reduce
+                                     (fn [acc n] (update-costs acc n node-idx data))
+                                     node-dist
+                                     node-nbrs)
+                                    node-idx)
               updated-processed (conj processed node-idx)
-              next-node (find-next-node updated-costs updated-processed)
-              next-nbrs (get graph next-node)]
+              next-node (find-next-node updated-costs)
+              next-nbrs (set/difference (get graph next-node) updated-processed)]
           (recur next-node next-nbrs updated-costs updated-processed))))))
 
-; slow ~12s :(
 (defn answer []
   (let [d15 (parse (read-data-safe "resources/d15.txt"))
         g15 (to-graph d15 100)]
@@ -114,16 +114,25 @@
 ;
 ;The entire cave is actually five times larger in both dimensions than you thought; the area you originally scanned is just one tile in a 5x5 tile area that forms the full map. Your original map tile repeats to the right and downward; each time the tile repeats to the right or downward, all of its risk levels are 1 higher than the tile immediately up or left of it. However, risk levels above 9 wrap back around to 1. So, if your original map had some position with a risk level of 8, then that same position on each of the 25 total tiles would be as follows:
 
-(defn grid [row col grid0]
-  (mapv #(if (> % 9) (- % 9) %) grid0))
+(defn inc-grid [grid inc]
+  (mapv #(if (> (+ inc %) 9) (- (+ inc %) 9) (+ inc %)) grid))
 
-(defn shortest-path-to-border [path-costs grid-size]
-  (apply merge
-         (map #(hash-map % (get path-costs %))
-       (concat (range (dec grid-size) (* grid-size grid-size) grid-size)
-               (range (* (dec grid-size) grid-size) (* grid-size grid-size))))))
+(defn enlarge [grid size factor]
+  (let [en-row (flatten (for [small-row (partition size grid)]
+                          (reduce
+                           (fn [acc i]
+                             (concat acc (inc-grid small-row i)))
+                           []
+                           (range factor))))
+        en-grid (flatten (reduce
+                          (fn [acc i]
+                            (concat acc (inc-grid en-row i)))
+                          []
+                          (range factor)))]
+    (vec en-grid)))
 
 (defn answer2 []
   (let [d15 (parse (read-data-safe "resources/d15.txt"))
-        g15 (to-graph d15 100)]
-    (time (get (dijkstra 0 g15 d15) 9999))))
+        ed15 (enlarge d15 100 5)
+        g15 (to-graph ed15 (* 5 100))]
+    (time (get (dijkstra 0 g15 ed15) 249999))))
